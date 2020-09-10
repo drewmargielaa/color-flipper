bd08ca2dfd925a4676c1eab44f8466f467c18dcf
Andrew Souza <drewmargielaa@gmail.com>
2020-09-10 17:07:57 -0400

    added random hex generator logic

--- hex.js
+++ hex.js
@@ -6,9 +6,13 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function() {
     let hexColor = "#";
     for(let i = 0; i < 6; i++) {
-        hexColor += hex[0];
+        hexColor += hex[getRandomNumber()];
     } 
     
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
