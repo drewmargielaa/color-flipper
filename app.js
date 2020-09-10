6b805fc942092582639e9f79aa3eb3427f18fab5
Andrew Souza <drewmargielaa@gmail.com>
2020-09-10 16:50:04 -0400

    added script to html, added math.floor to js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const colors = ["green", "red", "rgba(113,122,200)",
 "#f15025"];
 
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
 const color = document.querySelector(".color");
 
-btn.addEventListener('click', function(){
+btn.addEventListener("click", function(){
     // get random number between 0-3 
-    const randomNumber = 2;
+    const randomNumber = getRandomNumber();
+
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
